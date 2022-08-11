3cccd22864ddfc721665d979206c8236d87f94a8
Nesrine <nesrine.azaiez@esprit.tn>
2022-08-11 14:00:55 +0200

    creation projet stage

---
+++ src/test/java/com/nesrine/mobility/MobilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nesrine.mobility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobilityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
