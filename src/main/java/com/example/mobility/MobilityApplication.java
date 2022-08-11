a03a66bcf533c4b7c72e557a821ebbfdc789a6b7
Arfaoui11 <72455724+Arfaoui11@users.noreply.github.com>
2022-08-11 14:37:30 +0100

    asd

---
+++ src/main/java/com/example/mobility/MobilityApplication.java
@@ -0,0 +1,19 @@
+package com.example.mobility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableJpaRepositories
+@EnableScheduling
+@EnableSwagger2
+@SpringBootApplication
+public class MobilityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MobilityApplication.class, args);
+    }
+
+}
