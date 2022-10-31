5cf2d12b803c705447423d79f69eca06b49f0901
Arfaoui11 <72455724+Arfaoui11@users.noreply.github.com>
2022-10-31 16:05:53 +0100

    new

---
+++ src/main/java/com/example/mobility/service/ISendEmailService.java
@@ -0,0 +1,12 @@
+package com.example.mobility.service;
+
+import javax.mail.MessagingException;
+
+public interface ISendEmailService {
+
+ void sendSimpleEmail( String toEmail, String body, String subject);
+
+
+ void sendSimpleEmailWithFils( String toEmail, String body, String subject, String attchment) throws MessagingException;
+
+}
